d173521f0fcac17bf9b90d8530d31b2c5093f251
Erdem Badluev <erdembad@rambler.ru>
2026-02-20 23:27:21 +0200

    rewarded showed

--- build_yp.sh
+++ build_yp.sh
@@ -2,5 +2,6 @@
 
 rm -rf ./bin/yp.*xcframework && \
 ./scripts/generate_xcframework.sh yp debug 4.0 && \
-cp -r ./bin/yp.debug.xcframework /Users/erdembadluev/Documents/Projects/garage_game/ios/plugins/yp/ && \
-cp ./plugins/yp/yp.gdip /Users/erdembadluev/Documents/Projects/garage_game/ios/plugins/yp/ 
+cp -rf ./bin/yp.debug.xcframework /Users/erdembadluev/Documents/Projects/garage_game/ios/plugins/yp/ && \
+cp -f ./plugins/yp/yp.gdip /Users/erdembadluev/Documents/Projects/garage_game/ios/plugins/yp/ && \
+cp -rf ./bin/yp.debug.xcframework /Users/erdembadluev/Documents/Projects/garage_exported/Garage/dylibs/ios/plugins/yp/
--- plugins/yp/yp.mm
+++ plugins/yp/yp.mm
@@ -22,37 +22,76 @@
 YP *instance = NULL;
 id yb_instance;
 
-// Internal Objective-C class to handle Yandex callbacks
-// @interface GodotYP : NSObject <YMAInterstitialAdLoaderDelegate, YMAInterstitialAdDelegate>
-// @property (nonatomic, strong) YMAInterstitialAdLoader *interstitialAdLoader;
-// @property (nonatomic, strong) YMAInterstitialAd *interstitialAd;
-// @end
+@interface GodotYP : NSObject
+@end
 
-// @implementation GodotYP
-// - (instancetype)init {
-//     self = [super init];
-//     if (self) {
-//         _interstitialAdLoader = [[YMAInterstitialAdLoader alloc] init];
-//         _interstitialAdLoader.delegate = self;
-//     }
-//     return self;
-// }
+@implementation GodotYP
+- (instancetype)init {
+    self = [super init];
+    return self;
+}
 
-// // Delegate methods
-// - (void)interstitialAdLoader:(YMAInterstitialAdLoader *)adLoader didLoad:(YMAInterstitialAd *)interstitialAd {
-//     self.interstitialAd = interstitialAd;
-//     self.interstitialAd.delegate = self;
-// 	if(instance != NULL){
-//     	instance->emit_signal("interstitial_loaded");
-// 	}
-// }
+- (void) initialization_completed {
+	if(instance){
+		instance->emit_signal("initialization_completed");
+	}
+}
 
-// - (void)interstitialAdLoader:(YMAInterstitialAdLoader *)adLoader didFailToLoadWithError:(YMAAdRequestError *)error {
-//     if(instance != NULL){
-// 		instance->emit_signal("interstitial_failed_to_load", String(error.error.localizedDescription.UTF8String));
-// 	}
-// }
-// @end
+- (void) initialization_failed:(NSString *)error {
+	if(instance){
+		instance->emit_signal("initialization_failed", String(error.UTF8String));
+	}
+}
+
+- (void) interstitial_loaded {
+	if(instance){
+		instance->emit_signal("interstitial_loaded");
+	}
+}
+
+- (void) interstitial_failed_to_load:(NSString *)error {
+	if(instance){
+		instance->emit_signal("interstitial_failed_to_load", String(error.UTF8String));
+	}
+}
+
+- (void) interstitial_showed {
+	if(instance){
+		instance->emit_signal("interstitial_showed");
+	}
+}
+
+- (void) interstitial_failed_to_show:(NSString *)error {
+	if(instance){
+		instance->emit_signal("interstitial_failed_to_show", String(error.UTF8String));
+	}
+}
+
+- (void) rewarded_loaded {
+	if(instance){
+		instance->emit_signal("rewarded_loaded");
+	}
+}
+
+- (void) rewarded_failed_to_load:(NSString *)error {
+	if(instance){
+		instance->emit_signal("rewarded_failed_to_load", String(error.UTF8String));
+	}
+}
+
+- (void) rewarded_showed {
+	if(instance){
+		instance->emit_signal("rewarded_showed");
+	}
+}
+
+- (void) rewarded_failed_to_show:(NSString *)error {
+	if(instance){
+		instance->emit_signal("rewarded_failed_to_show", String(error.UTF8String));
+	}
+}
+
+@end
 
 YP *YP::get_singleton() {
 	return instance;
@@ -64,21 +103,25 @@ void YP::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("load_interstitial", "adUnitID"), &YP::load_interstitial);
 	ClassDB::bind_method(D_METHOD("show_interstitial"), &YP::show_interstitial);
 
+	ADD_SIGNAL(MethodInfo("init_completed"));
+	ADD_SIGNAL(MethodInfo("init_failed", PropertyInfo(Variant::STRING, "error")));
 	ADD_SIGNAL(MethodInfo("rewarded_loaded"));
-	ADD_SIGNAL(MethodInfo("rewarded_showed"));
 	ADD_SIGNAL(MethodInfo("rewarded_failed_to_load", PropertyInfo(Variant::STRING, "error")));
+	ADD_SIGNAL(MethodInfo("rewarded_showed"));
+	ADD_SIGNAL(MethodInfo("rewarded_failed_to_show", PropertyInfo(Variant::STRING, "error")));
 	ADD_SIGNAL(MethodInfo("interstitial_loaded"));
-	ADD_SIGNAL(MethodInfo("interstitial_showed"));
 	ADD_SIGNAL(MethodInfo("interstitial_failed_to_load", PropertyInfo(Variant::STRING, "error")));
+	ADD_SIGNAL(MethodInfo("interstitial_showed"));
+	ADD_SIGNAL(MethodInfo("interstitial_failed_to_show", PropertyInfo(Variant::STRING, "error")));
 }
 
 YP::YP() {
 	instance = this;
-	// [YMAMobileAds initializeSDKWithCompletionHandler:nil];
-	// godot_yp = [[GodotYP alloc] init];
 	Class cls = NSClassFromString(@"YBridge");
 	if(cls){
 		yb_instance = [[cls alloc] init];
+	} else {
+		NSLog(@"YBridge class not found. Make sure the YP plugin is properly integrated.");
 	}
 }
 
@@ -87,31 +130,43 @@ YP::~YP() {
 }
 
 void YP::load_rewarded(const String &ad_unit_id){
-
+	NSString *adId = [[NSString alloc] initWithUTF8String:ad_unit_id.utf8().get_data()];
+	NSLog(@"Loading rewarded ad with ID: %@", adId);
+	SEL selector = NSSelectorFromString(@"loadRewarded:");
+	if (yb_instance && [yb_instance respondsToSelector:selector]) {
+        [yb_instance performSelector:selector withObject:adId];
+    } else {
+		NSLog(@"YBridge instance does not respond to loadRewarded:");
+	}
 }
 
 void YP::show_rewarded(){
-
+	NSLog(@"Showing rewarded ad");
+	SEL selector = NSSelectorFromString(@"showRewarded:");
+	if (yb_instance && [yb_instance respondsToSelector:selector]) {
+        [yb_instance performSelector:selector withObject:0];
+    } else {
+		NSLog(@"YBridge instance does not respond to showRewarded:");
+	}
 }
 
 void YP::load_interstitial(const String &ad_unit_id){
 	NSString *adId = [[NSString alloc] initWithUTF8String:ad_unit_id.utf8().get_data()];
-	// YMAAdRequestConfiguration *config = [[YMAAdRequestConfiguration alloc] initWithAdUnitID:id];
-	// //[config initWithAdUnitID:id];
-	// [godot_yp.interstitialAdLoader loadAdWithRequestConfiguration:config];
+	NSLog(@"Loading interstitial ad with ID: %@", adId);
 	SEL selector = NSSelectorFromString(@"loadInterstitial:");
 	if (yb_instance && [yb_instance respondsToSelector:selector]) {
         [yb_instance performSelector:selector withObject:adId];
-    }
+    } else {
+		NSLog(@"YBridge instance does not respond to loadInterstitial:");
+	}
 }
 
 void YP::show_interstitial(){
-	// if(godot_yp.interstitialAd){
-	// 	UIViewController *root = [UIApplication sharedApplication].keyWindow.rootViewController;
-	// 	[godot_yp.interstitialAd showFromViewController:root];
-	// }
+	NSLog(@"Showing interstitial ad");
 	SEL selector = NSSelectorFromString(@"showInterstitial:");
 	if (yb_instance && [yb_instance respondsToSelector:selector]) {
         [yb_instance performSelector:selector];
-    }
+    } else {
+		NSLog(@"YBridge instance does not respond to showInterstitial:");
+	}
 }
